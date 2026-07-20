f311b168ab8b7226de953512dfa734091aec56a9
KM <the.real.laplace@gmail.com>
2026-07-20 20:49:00 +0300

    initial commit

--- src/from.js
+++ src/from.js
@@ -1,6 +1,6 @@
 const { nextToward } = require('@kmamal/util/ieee-float/double')
 
-const from = (start, end, openStart, openEnd) => [
+const from = (start, end, { openStart = false, openEnd = false } = {}) => [
 	openStart ? nextToward(start, Infinity) : start,
 	openEnd ? nextToward(end, -Infinity) : end,
 ]
